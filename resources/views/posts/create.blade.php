4d462196d363f89105d52ce88d27d375354ccdb5
koji0214 <koji02140117@gmail.com>
2022-09-12 21:39:39 +0900

    バリデーションの追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,34 +1,26 @@
-<!DOCTYPE html>
+
+<!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>CREATE</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <title>Blog</title>
     </head>
     <body>
+        <h1>Blog Name</h1>
         <form action="/posts" method="POST">
-            {{ csrf_field()  }}
+            @csrf
             <div class="title">
-                <h2>タイトル（〜30文字）</h2>
-                <input type="text" id="title" name="post[title]" required maxlength="30" size="30" placeholder="タイトル">
-            
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
-            
             <div class="body">
-                <h2>本文</h2>
-                <textarea type="text" id="content" name="post[body]" size="100"></textarea>
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="保存"/>
         </form>
-        
-
-        
-        <div class="back">
-            <a href="/">戻る</a>
-        </div>
+        <div class="back">[<a href="/">back</a>]</div>
     </body>
 </html>
