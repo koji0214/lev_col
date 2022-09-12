f602608b0b3a7dd0a38f45c74fd7f341e523f907
koji0214 <koji02140117@gmail.com>
2022-09-12 21:48:07 +0900

    編集画面の追加

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,8 @@ Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}','PostController@show');
 
 Route::post('/posts', 'PostController@store');
+
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
+
 ?>
